4039dda8c029f277c6b07f46265e46c9d4ec3811
Dan Lubbers <lubbers.dan@gmail.com>
2018-09-19 12:32:49 -0400

    finished lesson 12

--- basic-alogithm-scripting.js
+++ basic-alogithm-scripting.js
@@ -173,3 +173,13 @@ function titleCase(str) {
   console.log(titleCase("I'm a little tea pot"));
   console.log(titleCase("sHoRt AnD sToUt"));
   
+// 12. Slice and Splice
+
+function frankenSplice(arr1, arr2, n) {
+    // It's alive. It's alive!
+    let arrCopy = arr2.slice();
+    arrCopy.splice(n, 0, ...arr1)
+    return arrCopy;
+  }
+  
+  console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
